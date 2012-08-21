56bbfb8158d650f7c59b092e95e75f61332e544b
Cintia Higashi <cintiamh@gmail.com>
2012-08-21 02:12:39 -0400

    Kinda works...

--- space_invaders/js/script.js
+++ space_invaders/js/script.js
@@ -2,9 +2,13 @@
 var canvas;
 var ctx;
 var aliens;
-var aliens_imgs;
-var aliens_loaded;
 var createdAliens = false;
+var alien1a_img;
+var alien1b_img;
+var alien2a_img;
+var alien2b_img;
+var alien3a_img;
+var alien3b_img;
 
 $(document).ready(function(){
 	
@@ -14,14 +18,16 @@ $(document).ready(function(){
 	// draw background
 	ctx.fillStyle = "#000000";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
-	
-	/*aliens_imgs[0] = document.getElementById("img_alien1a");
-    aliens_imgs[1] = document.getElementById("img_alien1b");
-    aliens_imgs[2] = document.getElementById("img_alien2a");
-    aliens_imgs[3] = document.getElementById("img_alien2b");
-    aliens_imgs[4] = document.getElementById("img_alien3a");
-    aliens_imgs[5] = document.getElementById("img_alien3b");
 
+    alien1a_img = document.getElementById("img_alien1a");
+    alien1b_img = document.getElementById("img_alien1b");
+    alien2a_img = document.getElementById("img_alien2a");
+    alien2b_img = document.getElementById("img_alien2b");
+    alien3a_img = document.getElementById("img_alien3a");
+    alien3b_img = document.getElementById("img_alien3b");
+
+
+	/*
     aliens_imgs[0].onload = function() {
         aliens_loaded[0] = true;
     };
@@ -41,42 +47,54 @@ $(document).ready(function(){
         aliens_loaded[5] = true;
     };*/
 
-    setInterval(mainLoop, 30);
+    //createAliens();
+    drawAliens(0, 0);
+    //setInterval(mainLoop, 30);
 });
 
 function mainLoop() {
-    createAliens();
-    drawAliens();
+    //createAliens();
+    //drawAliens();
 }
 
 function createAliens() {
     if (!createdAliens) {
         var img_id;
         var img_width;
-        var img_height = 32;
         for (var i = 0; i < 5; i++) {
             for (var j = 0; j < 11; j++) {
                 if (i == 0) {
-                    img_id = "img_alien1";
+                    img_id = 1;
                     img_width = 32;
+                    alien1a_img.onload = function(){
+                        ctx.drawImage(alien1a_img, i * (32 + 24), j * (48 + 16));
+                    };
                 }
                 else if (i == 1 || i == 2) {
-                    img_id = "img_alien2";
+                    img_id = 2;
                     img_width = 44;
+                    alien2a_img.onload = function(){
+                        ctx.drawImage(alien2a_img, i * (32 + 24), j * (48 + 16));
+                    };
                 }
                 else if (i == 3 || i == 4) {
-                    img_id = "img_alien3";
+                    img_id = 3;
                     img_width = 48;
+                    alien3a_img.onload = function(){
+                        ctx.drawImage(alien3a_img, i * (32 + 24), j * (48 + 16));
+                    };
                 }
-                aliens.push ({
-                    x: i * (32 + 24),
-                    y: j * (48 + 16),
-                    w: img_width,
-                    h: img_height,
-                    img: img_id
-                });
+                //alert("createdAliens id: " + img_id + ", w: " + img_width + ", i: " + i + ", j: " + j);
+                //aliens.push ({
+                    //x: i * (32 + 24),
+                    //y: j * (48 + 16),
+                    //w: img_width,
+                    //h: 32,
+                    //img: img_id
+                //});
             }
         }
+        createdAliens = true;
     }
 }
 
@@ -84,13 +102,26 @@ function updateAliens() {
 
 }
 
-function drawAliens() {
-    var alien1_img = document.getElementById("img_alien1a");
-    var alien2_img = document.getElementById("img_alien2a");
-    var alien3_img = document.getElementById("img_alien3a");
-    for (var i = 0; i < aliens.length; i++) {
-        if (aliens[i].img_id == "img_alien1") {
-            ctx.drawImage(alien1_img, aliens[i].x, aliens[i].y);
+function drawAliens(xPos, yPos) {
+    if (xPos % 2 == 0) {
+        alien1a_img.onload = function(){
+            for (var i = 0; i < 11; i++) {
+                ctx.drawImage(alien1a_img, i * (32 + 24), 0);
+            }
+        };
+        alien2a_img.onload = function() {
+            for (var i = 1; i < 3; i++) {
+                for (var j = 0; j < 11; j++) {
+                    ctx.drawImage(alien2a_img, j * (32 + 24), i * (48 + 16));
+                }
+            }
+        }
+        alien3a_img.onload = function() {
+            for (var i = 3; i < 5; i++) {
+                for (var j = 0; j < 11; j++) {
+                    ctx.drawImage(alien3a_img, j * (32 + 24), i * (48 + 16));
+                }
+            }
         }
     }
 }
