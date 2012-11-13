510e07e677c9fca9dfabc36fbdbc2212731faedd
Cintia Higashi <cintiamh@gmail.com>
2012-11-12 23:00:04 -0500

    started pong game

---
+++ pong_html5/js/game.js
@@ -0,0 +1,137 @@
+// global variables
+var canvas;
+var ctx;
+var WIDTH = 800;
+var HEIGHT = 600;
+var PAD_WIDTH = 20;
+var PAD_HEIGHT = 100;
+var ball_pos = [WIDTH/2, HEIGHT/2];
+var ball_radius = 15;
+var ball_vel = [1, 1];
+var pad1_pos = [PAD_WIDTH/2, HEIGHT/2];
+var pad2_pos = [WIDTH - PAD_WIDTH/2, HEIGHT/2];
+var pad1_vel = [0, 0];
+var pad2_vel = [0, 0];
+
+function drawCircle(x, y, r) {
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, Math.PI * 2, true);
+    ctx.fill();
+}
+
+function drawRect(x, y, w, h) {
+    ctx.beginPath();
+    ctx.rect(x, y, w, h);
+    ctx.closePath();
+    ctx.fill();
+}
+
+function drawTable() {
+    ctx.beginPath();
+    ctx.moveTo(WIDTH/2, 0);
+    ctx.lineTo(0, 0);
+    ctx.lineTo(0, HEIGHT);
+    ctx.lineTo(WIDTH, HEIGHT);
+    ctx.lineTo(WIDTH, 0);
+    ctx.lineTo(WIDTH/2, 0);
+    ctx.lineTo(WIDTH/2, HEIGHT);
+    ctx.moveTo(PAD_WIDTH, 0);
+    ctx.lineTo(PAD_WIDTH, HEIGHT);
+    ctx.moveTo(WIDTH - PAD_WIDTH, 0);
+    ctx.lineTo(WIDTH - PAD_WIDTH, HEIGHT);
+    ctx.closePath();
+    ctx.stroke();
+}
+
+function checkKeyDown(event) {
+    switch(event.keyCode) {
+        case 38: // Up arrow
+            if ((pad2_pos[1] - PAD_HEIGHT / 2) > 0) {
+                pad2_vel[1] = -2;
+            }
+            else {
+                pad2_vel[1] = 0;
+            }
+            break;
+        case 40: // Down arrow
+            if ((pad2_pos[1] + PAD_HEIGHT /2) < HEIGHT) {
+                pad2_vel[1] = 2;
+            }
+            else {
+                pad2_vel[1] = 0;
+            }
+            break;
+        case 87: // W
+            if ((pad1_pos[1] - PAD_HEIGHT / 2) > 0) {
+                pad1_vel[1] = -2;
+            }
+            else {
+                pad1_vel[1] = 0;
+            }
+            break;
+        case 83: // S
+            if ((pad1_pos[1] + PAD_HEIGHT /2) < HEIGHT) {
+                pad1_vel[1] = 2;
+            }
+            else {
+                pad1_vel[1] = 0;
+            }
+            break;
+    }
+}
+
+function checkKeyUp(event) {
+    switch(event.keyCode) {
+        case 38: // Up arrow
+            if ((pad2_pos[1] - PAD_HEIGHT / 2) > 0) {
+                pad2_vel[1] = 0;
+            }
+            break;
+        case 40: // Down arrow
+            if ((pad2_pos[1] + PAD_HEIGHT /2) < HEIGHT) {
+                pad2_vel[1] = 0;
+            }
+            break;
+    }
+}
+
+function clear() {
+    ctx.clearRect(0, 0, WIDTH, HEIGHT);
+}
+
+function init() {
+    canvas = document.getElementById("game_window");
+    ctx = canvas.getContext("2d");
+    return setInterval(draw, 10);
+}
+
+function updatePositions() {
+    ball_pos[0] += ball_vel[0];
+    ball_pos[1] += ball_vel[1];
+    //pad1_pos[0] += pad1_vel[0];
+    pad1_pos[1] += pad1_vel[1];
+    //pad2_pos[0] += pad2_vel[0];
+    pad2_pos[1] += pad2_vel[1];
+}
+
+function draw() {
+    clear();
+    ctx.fillStyle = "#000000";
+    drawRect(0, 0, WIDTH, HEIGHT);
+    ctx.fillStyle = "#FFFFFF";
+    ctx.strokeStyle = "#FFFFFF";
+    drawTable();
+
+    updatePositions();
+
+    drawCircle(ball_pos[0], ball_pos[1], ball_radius);
+    // draw left paddle
+    drawRect(0, pad1_pos[1] - PAD_HEIGHT/2, PAD_WIDTH, PAD_HEIGHT);
+    // draw right paddle
+    drawRect(WIDTH - PAD_WIDTH, pad2_pos[1] - PAD_HEIGHT/2, PAD_WIDTH, PAD_HEIGHT);
+}
+
+init();
+
+window.addEventListener('keydown', checkKeyDown, true);
+window.addEventListener('keyup', checkKeyUp, true);
